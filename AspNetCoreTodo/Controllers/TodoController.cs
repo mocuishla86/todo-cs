f93a6316d97b4a6ddb67cff97f7e91be10b5ef1d
isabel.llanes <isabel.llanes@thougthworks.com>
2021-09-23 22:13:10 +0200

    initial commit

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+    public IActionResult Index()
+    {
+        // Obtener las tareas desde la base de datos
+
+        // Colocar los tareas en un modelo
+
+        // Genera la vista usando el modelo
+    }
+    }
+}
---
+++ AspNetCoreTodo/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; }
+    }
+
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
